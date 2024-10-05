cc62ad82afabd5f4645a85cfd69e7f16bff3bf51
Java Kanaya Prada <javakanaya@gmail.com>
2024-10-06 01:13:39 +0700

    feat: styling text and background colors

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 54, 244, 174),
+              Color.fromARGB(255, 16, 83, 137)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              "Hello World!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
