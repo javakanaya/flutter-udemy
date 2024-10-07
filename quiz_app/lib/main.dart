d31914858302698ed196ba6714a88954f790c682
Java Kanaya Prada <javakanaya@gmail.com>
2024-10-07 22:18:53 +0700

    feat: start screen

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 19, 110, 184),
+                Color.fromARGB(255, 15, 48, 169)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
