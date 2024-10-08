28450f1b8d4456360e7061fbf7374172f61ceb86
Java Kanaya Prada <javakanaya@gmail.com>
2024-10-08 22:06:18 +0700

    feat: more style on start screen widget

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -9,9 +9,24 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          /**
+           * The Opacity widget is used to make its child widget semi-transparent.
+           * But avoid it, as it can be expensive to render.
+           */
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
+          /**
+           * Instead use this method
+           */
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(149, 255, 255, 255),
           ),
           const SizedBox(height: 80),
           const Text(
@@ -22,12 +37,15 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
-              onPressed: () {},
+          OutlinedButton.icon(
+              onPressed: () {
+                // ...
+              },
               style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.white,
               ),
-              child: const Text("Start Quiz")),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text("Start Quiz")),
         ],
       ),
     );
