27399faaf7d5b1ac1c2a19c8b6713015384f490e
Java Kanaya Prada <javakanaya@gmail.com>
2024-11-01 22:55:17 +0700

    refactor: move the smalles changing state section to a new stateful widget

---
+++ flutter_internal/lib/demo_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+// keep your stateful widget as small as possible
+class DemoButtons extends StatefulWidget {
+  const DemoButtons({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DemoButtonState();
+  }
+}
+
+class _DemoButtonState extends State<DemoButtons> {
+  var _isUnderstood = false;
+
+  @override
+  Widget build(BuildContext context) {
+    print('DemoButtons BUILD called');
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isUnderstood = false;
+                });
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _isUnderstood = true;
+                });
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        ),
+        if (_isUnderstood) const Text('Awesome!'),
+      ],
+    );
+  }
+}
