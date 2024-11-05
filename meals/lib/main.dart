dc54418442544d845f372482391e60df68605020
Java Kanaya Prada <javakanaya@gmail.com>
2024-11-05 20:54:14 +0700

    feat: cross screen navigation

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
@@ -23,7 +21,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(title: 'some category', meals: dummyMeals),
+      home: const CategoriesScreen(),
     );
   }
 }
