4a8a44fdaa5ad91fedc01f23f6d3d82c987b8448
Java Kanaya Prada <javakanaya@gmail.com>
2024-11-10 11:31:26 +0700

    feat: disable drawer on the filters screen

--- meals/lib/screens/filters.dart
+++ meals/lib/screens/filters.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/tabs.dart';
-import 'package:meals/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -18,16 +16,19 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      // ! Adding a drawer to the filters screen
+      /* 
       drawer: MainDrawer(onSelectScreen: (identifier) {
         Navigator.of(context).pop();
         if (identifier == 'meals') {
-          Navigator.of(context).push(
+          Navigator.of(context).pushReplacement(
             MaterialPageRoute(
               builder: (ctx) => const TabsScreen(),
             ),
           );
         }
       }),
+      */
       body: Column(
         children: [
           SwitchListTile(
