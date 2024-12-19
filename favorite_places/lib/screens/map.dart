c09e6fb442e104fa68a036f02a3315edc6635d6b
Java Kanaya Prada <javakanaya@gmail.com>
2024-12-19 22:57:06 +0700

    feat: select place on map

--- favorite_places/lib/screens/map.dart
+++ favorite_places/lib/screens/map.dart
@@ -24,6 +24,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,30 +34,43 @@ class _MapScreenState extends State<MapScreen> {
         actions: [
           if (widget.isSelecting)
             IconButton(
-              onPressed: () {},
               icon: const Icon(Icons.save),
+              onPressed: () {
+                Navigator.of(context).pop(_pickedLocation);
+              },
             )
         ],
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(
-            widget.location.latitude,
-            widget.location.longitude,
-          ),
-          zoom: 16,
-        ),
-        //  a set data type, using curly braces
-        markers: {
-          Marker(
-            markerId: const MarkerId('m1'),
-            position: LatLng(
-              widget.location.latitude,
-              widget.location.longitude,
+      body: !widget.isSelecting
+          ? null
+          : GoogleMap(
+              onTap: (position) {
+                setState(() {
+                  _pickedLocation = position;
+                });
+              },
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  widget.location.latitude,
+                  widget.location.longitude,
+                ),
+                zoom: 16,
+              ),
+              //  a set data type, using curly braces
+              markers: (_pickedLocation == null && widget.isSelecting)
+                  // showing nothing if no picked location
+                  ? {}
+                  : {
+                      Marker(
+                        markerId: const MarkerId('m1'),
+                        position: _pickedLocation ??
+                            LatLng(
+                              widget.location.latitude,
+                              widget.location.longitude,
+                            ),
+                      ),
+                    },
             ),
-          ),
-        },
-      ),
     );
   }
 }
