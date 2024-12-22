6281bb217794c8217cd0ebd82f186b3535f09377
Java Kanaya Prada <javakanaya@gmail.com>
2024-12-22 21:15:49 +0700

    feat: store the places data to the local sqlite database

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -4,6 +4,8 @@ import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   // set initial state to empty array
@@ -19,6 +21,26 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       image: copiedImage,
       location: location,
     );
+
+    // initalize the database
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'), // db name should ended with .db
+      onCreate: (db, version) {
+        return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+
+    // store the metadata to the local database
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address
+    });
 
     // update the state by creating a new array, instead of edit it
     state = [
